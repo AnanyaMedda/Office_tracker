e5049e728e53443d12def121b7d7396e694495f0
Ananya Medda <ananyamedda2@gmail.com>
2026-04-17 14:10:04 +0530

    Fix: Enabled config.js and pushed unbreakable data logic for live site

---
+++ config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyApW_7EdKZtGWrv1yLvwraiSFJUkz9svhg",
+  authDomain: "office-attendance-tracke-3f3db.firebaseapp.com",
+  projectId: "office-attendance-tracke-3f3db",
+  storageBucket: "office-attendance-tracke-3f3db.firebasestorage.app",
+  messagingSenderId: "74594316196",
+  appId: "1:74594316196:web:02f2aaaa343469a76e9b31"
+};
